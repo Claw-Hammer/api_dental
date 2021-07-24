03d88713371049cff0409f613d99d84d858d1fc8
ClawHammer <jvca2005@gmail.com>
2021-07-24 16:08:55 -0400

    Creating the users register controller (into the Api sub folder)

---
+++ app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|string|min:8'
+        ]);
+
+        $user = User::create($request->all());
+        return response($user, 200);
+    }
+}
