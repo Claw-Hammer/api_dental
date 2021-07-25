9f8b7399a76ce6235867e90c8a33ebee2c9ea3cd
ClawHammer <jvca2005@gmail.com>
2021-07-25 16:40:54 -0400

    Hashing the password before create the user into the DB

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -5,18 +5,20 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     public function store(Request $request)
     {
-        $request->validate([
+        $registerData = $request->validate([
             'name' => 'required|string|max:100',
             'email' => 'required|string|email|max:100|unique:users',
             'password' => 'required|string|string|min:8'
         ]);
 
-        $user = User::create($request->all());
+        $registerData['password'] = Hash::make($request->password);
+        $user = User::create($registerData);
         return response($user, 200);
     }
 }
