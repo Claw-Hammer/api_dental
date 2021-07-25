c19eb97b4b1f39119c32fd2485c4bbb6e96adaaa
ClawHammer <jvca2005@gmail.com>
2021-07-25 17:27:22 -0400

    Registering the show user info controller into the api protected routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use \App\Http\Controllers\Api\LoginController;
+use \App\Http\Controllers\Api\ShowUserInfoController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,6 +15,6 @@ Route::group(['prefix' => 'v1/users'], function () {
     Route::post('/login', [LoginController::class, 'login'])->name('api.v1.login');
 
     Route::group(['middleware' => ['auth:api']], function() {
-
+        Route::get('/show', [ShowUserInfoController::class, 'show'])->name('api.v1.showUserInfo');
     });
 });
