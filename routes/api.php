d9f2f41a92ca819f26c69a20c703e5304fa2c3a1
ClawHammer <jvca2005@gmail.com>
2021-07-26 00:22:17 -0400

    Generatins the notifications resource Route; changing the routes names

--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use \App\Http\Controllers\Api\LoginController;
 use \App\Http\Controllers\Api\ShowUserInfoController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-});
+use \App\Http\Controllers\Api\NotificationController;
 
 Route::group(['prefix' => 'v1/users'], function () {
-    Route::post('/register', [RegisterController::class, 'store'])->name('api.v1.register');
-    Route::post('/login', [LoginController::class, 'login'])->name('api.v1.login');
+    Route::post('/register', [RegisterController::class, 'store'])->name('api.v1.user.register');
+    Route::post('/login', [LoginController::class, 'login'])->name('api.v1.user.login');
 
     Route::group(['middleware' => ['auth:api']], function() {
-        Route::get('/show', [ShowUserInfoController::class, 'show'])->name('api.v1.showUserInfo');
+        //users info
+        Route::get('/show', [ShowUserInfoController::class, 'show'])->name('api.v1.user.show');
+        //notifications
+        Route::apiResource('/notifications', NotificationController::class)->names('api.v1.notifications');
     });
 });
