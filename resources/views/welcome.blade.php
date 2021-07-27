5a2094a197c6d4a9734a55b76c2b062b97a86119
Julio <jvca2005@gmail.com>
2021-07-27 15:55:15 -0400

    Api Dental v1.1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -142,7 +142,7 @@
                     </div>
 
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }}) API Dental v1.1
                     </div>
                 </div>
             </div>
